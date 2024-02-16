f59a24eff11cc02850c90e548c7ee47e6b776290
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-02-16 12:48:54 +0200

    intialised variables in js file

---
+++ blackjack.js
@@ -0,0 +1,15 @@
+let player = {
+    name: "Miguel",
+    chips: 150
+
+}
+
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let playerEl = document.querySelector("#player-el")
+let cardsEl = document.querySelector("#cards-el")
+let sumEl = document.querySelector("#sum-el")
+let messageEl = document.querySelector("#message-el")
